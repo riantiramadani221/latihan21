9bc9e740a27bfae87fc9a7b24187aaebec89eff5
riantiramadani221 <riantiramadani221@gmail.com>
2026-04-14 10:29:27 +0700

    toko kasir versi 5

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-let dataBarang = [] //aray global untuk menyimpan data barang 
+// array gelobal untuk menyimpan data barang 
+// sekarang akan menyimpan 
+// [nama,harga],[nama,harga], dst...
+let dataBarang = [] 
 
 // fungsi format rupiah 1.000.000
 function rupiah(angka) {
@@ -7,13 +10,20 @@ function rupiah(angka) {
 
 // fungsi untuk menambah barang ke dalam keranjang
 function tambah() {
+  //mengambil data nama dari elemen input dengan id "namabaranginput"
+    let nama =document.getElementById("namabaranginput").value
+    
   //mengambil data harga dari elemen input dengan id "hargainput"
   //dan mengubah menjadi angka
   let harga = Number(document.getElementById("hargainput").value)
   
   //masukkan data harga ke dalam array databarang
-  dataBarang.push(harga)
+  dataBarang.push([nama,harga]) 
   
+  // kosongkan input harga setelah ditambahkan
+  document.getElementById("namabaranginput").value = ""
+  
+   // kosongkan input harga setelah ditambahka
   document.getElementById("hargainput").value = ""
   
   //tampilkan data harga barang
@@ -26,10 +36,13 @@ function tampilkan() {
   
   //loop input harga barang dari array global data barang
   for (let i = 0; i < dataBarang.length; i++) {
-    let harga = dataBarang[i]
+    //dataBarang[i][0] adalah nama barang
+    //dataBarang[i][1] adalah haraga barang
+    let nama = dataBarang[i][0]
+     let harga = dataBarang[i][1]
     total += harga
     
-    output += "Barang ke-" + (i + 1) + ": Rp " + rupiah(harga)
+    output +=(i + 1) + "."+ nama + ": RP "+rupiah(harga)
     +"<button onclick='hapusindex("+ i +")'class='hapus2'>Hapus</button>"
     + "<br>"
   }
@@ -51,16 +64,20 @@ function reset() {
 // fungsi untuk mengedit harga barang berdasarkan nomer barang 
 function edit() {
   
-  // ubah barang menjadi index array dengan mengurangi 1
+  // ubah nomor barang menjadi index array dengan mengurangi 1
   let index = Number(document.getElementById("nomor").value)-1
   
-  // Ambil harga edit dari input 
-  let harga = Number(document.getElementById("hargaedit").value)
+  // Ambil nama edit dari input 
+  let nama = document.getElementById("namabarangedit").value
+    // Ambil harga edit dari input 
+  let harga= Number (document.getElementById("hargaedit").value)
   
   // periksa apakah index valid
   if (index >= 0 && index < dataBarang.length) {
-    // update harga barang di araay dataBarang 
-    dataBarang[index] = harga
+    //update harga barang di array dataBarang
+    dataBarang[index][0]= nama
+    dataBarang[index][1]= harga 
+    
   } else {
     //jika nomer barang tidak valid, tampilan alert
     alert("Nomor barang tidak valid ")
@@ -70,6 +87,7 @@ function edit() {
     //kosongkan input nomor dan harga edit setelah diedit 
     document.getElementById("nomor").value =""
     document.getElementById("hargaedit").value=""
+    document.getElementById("namabarangedit").value=""
 }
 
 //fungsi untuk menghapus barang berdasarkan nomor barang 
@@ -79,8 +97,10 @@ let index = Number(document.getElementById("nomerHapus"). value)-1
 
   // periksa apakah index valid
   if (index >= 0 && index < dataBarang.length) {
+
   //hapus barang dari array dataBarang menggunakan splice 
   dataBarang.splice(index,1)
+  
   } else {
     //jika nomer barang tidak valid, tampilan alert
     alert("Nomor barang tidak valid ")
